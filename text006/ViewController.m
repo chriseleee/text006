c91954120d1af15e15e691bf14565c8e5c2d360a
chrise <gongjunping@chris.local>
2018-03-05 13:03:24 +0800

    第一次提交代码

---
+++ text006/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  text006
+//
+//  Created by chrise on 2018/3/5.
+//  Copyright © 2018年 chrise. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
